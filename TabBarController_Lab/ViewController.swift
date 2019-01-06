8813cb3d7ceb264a152e3a8a3b34a4568dd6f696
Dmitry Novosyolov <i1710@icloud.com>
2019-01-07 01:14:45 +0200

    Initial Commit

---
+++ TabBarController_Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarController_Lab
+//
+//  Created by Dim on 07/01/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
